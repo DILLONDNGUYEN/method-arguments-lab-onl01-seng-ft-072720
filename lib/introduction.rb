40c9e607cc0d34c09a1cd78a8635c177a4ff111c
Dillon Nguyen <dillon.d.nguyen@gmail.com>
2020-07-27 18:59:37 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,5 +4,5 @@ end
 introduction
 
 def introduction_with_language(name, language)
-  puts "Hi, my name is #{name} and Iam learning to program in #{language}."
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
